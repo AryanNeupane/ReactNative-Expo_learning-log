aae26525b937e94ac211f16f79eb503031f1f2f2
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-28 23:20:24 +0545

    learning tabs

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,13 +1,17 @@
 import { View, Text } from 'react-native'
+import { Link } from 'expo-router'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const explore = () => {
+const account = () => {
   return (
     <SafeAreaView>
-      <Text>explore</Text>
+      <Text>account</Text>
+      <Link href={"/accountInfo"}>
+        <Text>Accounts Here</Text>
+      </Link>
     </SafeAreaView>
   )
 }
 
-export default explore
+export default account
